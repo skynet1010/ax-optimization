197f279848e4e55699ce16c04bcfa59c118122a1
Andreas Klos <andreas.klos@fernuni-hagen.de>
2020-05-28 16:59:12 +0200

    FIX bug, str object not callable

--- code/BOBO_hypterparameter_search.py
+++ code/BOBO_hypterparameter_search.py
@@ -54,7 +54,7 @@ def objective(parameters):
     train_data_loader, valid_data_loader, test_data_loader = get_dataloaders(args,ss,data_composition_key, model_key)
     model = manipulateModel(model_key,args.is_feature_extraction,data_compositions[data_composition_key])
     criterion = parameters.get("criterion")
-    optimizer = parameters.get("optimizer")(model.parameters(), lr=parameters.get("lr"),weight_decay=parameters.get("weight_decay"))
+    optimizer = optimizer_dict[parameters.get("optimizer")](model.parameters(), lr=parameters.get("lr"),weight_decay=parameters.get("weight_decay"))
 
     best_loss = sys.float_info.max
     best_acc = 0.0
