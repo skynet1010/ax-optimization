b2ed62ec7ef789932be917281cb8bdcf1dbcc4ea
Andreas Klos <andreas.klos@fernuni-hagen.de>
2020-05-28 13:57:47 +0000

    ADJUST taskqueue name

--- code/utils/messagebroker_new_tasks.py
+++ code/utils/messagebroker_new_tasks.py
@@ -16,13 +16,13 @@ def create_new_tasks(fn,broker):
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=get_valid_ip(broker)))
     channel = connection.channel()
 
-    channel.queue_declare(queue='task_queue', durable=True)
+    channel.queue_declare(queue='task_queue_ho', durable=True)
 
     for task in tasks:
         task = task.strip("\n")
         channel.basic_publish(
                 exchange='',
-                routing_key='task_queue',
+                routing_key='task_queue_ho',
                 body=task,
                 properties=pika.BasicProperties(
                     delivery_mode=2,  # make message persisten
