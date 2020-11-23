eed75936bde7d64cd2d1603ec1fb245660077c2d
Andreas Klos <andreas.klos@fernuni-hagen.de>
2020-11-23 15:41:40 +0100

    ADJUST naming fn name

--- code/utils/dataloader_provider.py
+++ code/utils/dataloader_provider.py
@@ -12,26 +12,11 @@ from numpy import floor
 def get_dataloaders(args,ss, data_composition_key,model_key,validation=True):
 
     input_filename = f"train_test_data_{ss}_supervised.hdf5"
-    real_data_path = os.path.join("..","data")
-    if not os.path.isdir(real_data_path):
-        try:
-            os.mkdir(real_data_path)
-        except Exception as e:
-            print(e)
-            exit(1)
-
-
-        
-    full_real_input_filename = os.path.join(real_data_path,input_filename)
-    if not os.path.isfile(full_real_input_filename):
-        try:
-            shutil.copy(os.path.join(args.data_dir,"{}".format(input_filename)),full_real_input_filename)
-        except Exception as e:
-            print(e)
-            exit(1)
-
-    train_ds = Dataset(full_real_input_filename, "supervised","training",data_composition_key,model_key)
-    test_ds = Dataset(full_real_input_filename, "supervised","test",data_composition_key, model_key)
+    
+    full_input_filename = os.path.join(args.data_dir,"{}".format(input_filename))
+    
+    train_ds = Dataset(full_input_filename, "supervised","training",data_composition_key,model_key)
+    test_ds = Dataset(full_input_filename, "supervised","test",data_composition_key, model_key)
     cpu_count = multiprocessing.cpu_count()
 
     test_data_loader = torch.utils.data.DataLoader(test_ds,num_workers=cpu_count,batch_size=args.batch_size,pin_memory=True,shuffle=False)
